805ee1e6be0d62fa3d35be6a7a20bf5047230d15
MauroBorzi <mauroborzi91@gmail.com>
2025-08-25 15:02:46 +0200

    [upd]header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 const Header = () => {
   return (
     <header>
-      <h1>ciao sono header </h1>
+      <div className="container-h">
+        <img src="img/dc-logo.png" alt="" />
+        <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a className="active" href="#">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
+      </div>
     </header>
   )
 }
