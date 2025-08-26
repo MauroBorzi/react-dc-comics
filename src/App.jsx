2bbf4e626e5950d103bad25f3048322514e800bd
MauroBorzi <mauroborzi91@gmail.com>
2025-08-26 17:02:09 +0200

    [add]jumbo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // importazione delle componenti
 import Header from "./components/header"
+import Jumbo from "./components/Jumbo"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Jumbo />
       <Main />
       <Footer />
     </>
---
+++ src/components/Jumbo.jsx
@@ -0,0 +1,11 @@
+const Jumbo = () => {
+  return (
+    <div className="jumbo">
+      <div className="absolute">
+        <button type="button" className="button2"><h4>CURRENT SERIES</h4></button>
+      </div>
+    </div>
+  )
+}
+
+export default Jumbo
